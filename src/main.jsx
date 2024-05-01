cf1ebe4efa730cf0985d78958667f17d947f5ef1
Anoop Wilson <anoopwilson33@yahoo.com>
2024-05-01 11:19:17 +0530

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
 import './index.css'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Home from './routes/home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+    
+      },
+    ],
+
+    // children: [
+    //   {
+    //     path: "contacts/:contactId",
+    //     element: <Contact />,
+    //   },
+    // ],
+
+    // <Link to={`contacts/1`}>Your Name</Link>
+
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
