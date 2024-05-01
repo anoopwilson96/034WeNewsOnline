112106f95e72e99e18a4db16299d75e399c3adc9
Anoop Wilson <anoopwilson33@yahoo.com>
2024-05-01 11:18:19 +0530

    added root router

---
+++ src/routes/root.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+import Header from './header';
+import Footer from './footer';
+
+export default function Root() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer/>
+    
+    </>
+  )
+}
