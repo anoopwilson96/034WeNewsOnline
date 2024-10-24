8182f57eaf8ecf69a1d717be0464974828323268
Anoop Wilson <anoopwilson33@yahoo.com>
2024-10-24 17:35:32 +0530

    Update newsArticle.jsx

--- src/routes/newsArticle.jsx
+++ src/routes/newsArticle.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
+// import React from 'react'
 
 
 
 
 
-export default function NewsArticle() {
-  return (
-    <>
-  <div className='border border-t-4 '>
-    <div className="container mx-auto flex-1 justify-center max-w-6xl">
+// export default function NewsArticle() {
+//   return (
+//     <>
+//   <div className='border border-t-4 '>
+//     <div className="container mx-auto flex-1 justify-center max-w-6xl">
 
     
-       <div className="  blog-card m-5  p-3 border rounded-md border-slate-400  flex flex-row justify-center align-middle items-center ">
-       <img className='p-5 object-contain mb-5 max-h-55 w-96 flex flex-row justify-center align-middle' src="../dummy image.png" alt="" srcSet="" />
+//        <div className="  blog-card m-5  p-3 border rounded-md border-slate-400  flex flex-row justify-center align-middle items-center ">
+//        <img className='p-5 object-contain mb-5 max-h-55 w-96 flex flex-row justify-center align-middle' src="../dummy image.png" alt="" srcSet="" />
 
-      </div>
-      <div className="  blog-card m-5  p-3 border rounded-md border-slate-400  flex flex-col justify-center align-middle ">
+//       </div>
+//       <div className="  blog-card m-5  p-3 border rounded-md border-slate-400  flex flex-col justify-center align-middle ">
 
 
-          <h2 className="text-xl font-semibold mt-3  ">News title</h2>
-          <h3 className="mt-2 mb-2"> Description about the news article div will be here.</h3>
+//           <h2 className="text-xl font-semibold mt-3  ">News title</h2>
+//           <h3 className="mt-2 mb-2"> Description about the news article div will be here.</h3>
       
 
-        <a href="#" className='self-center'><button className="  bg-slate-400 p-2 border-none rounded-full m-2 cursor-pointer">
-        Know More
-        </button></a>
-      </div>
+//         <a href="#" className='self-center'><button className="  bg-slate-400 p-2 border-none rounded-full m-2 cursor-pointer">
+//         Know More
+//         </button></a>
+//       </div>
       
-    </div>
-  </div>
+//     </div>
+//   </div>
   
 
     
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
