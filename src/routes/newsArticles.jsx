623567dd74cc717b4f3df48f71f06e8faf9d2078
Anoop Wilson <anoopwilson33@yahoo.com>
2024-10-24 12:49:08 +0530

    Added .env file

--- src/routes/newsArticles.jsx
+++ src/routes/newsArticles.jsx
@@ -4,7 +4,7 @@ import { Outlet, Link,useLoaderData, } from "react-router-dom";
 
 
 export async function loader() {
-  const response = await fetch('https://gnews.io/api/v4/search?q=example&lang=en&country=in&max=10&apikey=f0168094ea9df2f5fae1551f9a16e325')
+  const response = await fetch('https://gnews.io/api/v4/search?q=example&lang=en&country=in&max=10&apikey=')
   const data = await response.json()
   return {data}
 }
@@ -23,12 +23,12 @@ export default function NewsArticles() {
 
       <div
         id="blog-container"
-        className="container lg:container justify-items-center m-auto sectionThree grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1"
+        className="container lg:container  m-auto sectionThree grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1"
       >
         {data.articles.map((article, index) => (
           <div
             key={article.id || index}
-            className="blog-card max-w-64 m-5 cursor-pointer p-3 border rounded-md border-slate-400 flex flex-col justify-between align-middle"
+            className=" max-w-64 m-5 cursor-pointer p-3 border rounded-md border-slate-400 flex flex-col  align-middle"
           >
             <img className='max-h-32' src={article.image} alt={article.title} />
             <h2 className="text-xl font-semibold mt-3">{article.title}</h2>
