8aff0c9c7dbfa223a86e881442f3928a3352b1ee
Anoop Wilson <anoopwilson33@yahoo.com>
2024-11-10 09:42:42 +0530

    Update newsAPI.js

--- src/service/newsAPI.js
+++ src/service/newsAPI.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
-async function getNews() {
+
+export const getNews = async (query,country)=> {
   try {
-    const query = "ircc";
-    const country = "ca" ;
+
     const response = await axios.get(`https://gnews.io/api/v4/search?q=${query}&lang=en&country=${country}&max=10&apikey=${import.meta.env.VITE_API_URL0}`);
     console.log(response);
+    return  response
   } catch (error) {
     console.error(error);
+    return null;
   }
 }
+
+
+
