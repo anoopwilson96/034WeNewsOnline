d6b24f594aa09757fe59866073cee1bfe5b3423a
Anoop Wilson <anoopwilson33@yahoo.com>
2024-11-10 12:46:20 +0530

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Outlet } from "react-router-dom";
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 export default function Root() {
+  const [country, setCountry] = useState('gb');
+  const [query, setQuery] = useState('example');
+
+  const handleUpdate = ({ country: newCountry, query: newQuery }) => {
+    if (newCountry !== undefined) setCountry(newCountry);
+    if (newQuery !== undefined) setQuery(newQuery);
+  };
+
   return (
     <>
-    <Header/>
-    <Outlet />
-    <Footer/>  
+      <Header onUpdate={handleUpdate} />
+      <Outlet context={{ country, query }} />
+      <Footer />
     </>
-  )
+  );
 }
